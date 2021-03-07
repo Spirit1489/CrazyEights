a651fd9a92c8fe9a22a7ea9dc72638bc9cf6c728
Spirit1489 <Spirit1489@gmail.com>
2021-03-07 05:55:58 +0300

    Add PlayButton

--- app/src/main/java/ru/spiritblog/crazyeights/SplashScreen.kt
+++ app/src/main/java/ru/spiritblog/crazyeights/SplashScreen.kt
@@ -12,13 +12,22 @@ private const val TAG = "SplashScreen"
 
 public class SplashScreen(context: Context) : View(context) {
 
-
+    // Title
     private lateinit var titleG: Bitmap
     private var scrW: Int = 0
     private var scrH: Int = 0
 
+    // PlayButton
+    private lateinit var playBtnUp: Bitmap
+    private lateinit var playBtnDn: Bitmap
+    private var playBtnPressed: Boolean = false
+
+
     init {
         titleG = BitmapFactory.decodeResource(resources, R.drawable.splash_graphic)
+
+        playBtnUp = BitmapFactory.decodeResource(resources, R.drawable.btn_up)
+        playBtnDn = BitmapFactory.decodeResource(resources, R.drawable.btn_down)
     }
 
 
@@ -32,28 +41,68 @@ public class SplashScreen(context: Context) : View(context) {
         super.onDraw(canvas)
         var titleGLeftPos = (scrW - titleG.width) / 2
         canvas?.drawBitmap(titleG, titleGLeftPos.toFloat(), 100F, null)
+
+        var playBtnLeftPos = (scrW - playBtnUp.width) / 2
+        if (playBtnPressed) {
+            canvas?.drawBitmap(
+                playBtnDn, playBtnLeftPos.toFloat(),
+                (scrH * 0.5).toFloat(), null
+            )
+        } else {
+            canvas?.drawBitmap(
+                playBtnUp, playBtnLeftPos.toFloat(),
+                (scrH * 0.5).toFloat(), null
+            )
+        }
+
     }
 
 
-//    override fun onTouchEvent(evt: MotionEvent): Boolean {
-//
-//        var action = evt.action
-//
-//        when (action) {
-//
-//            MotionEvent.ACTION_DOWN -> Log.d(TAG, "Down")
-//            MotionEvent.ACTION_UP -> Log.d(TAG, "Up")
-//            MotionEvent.ACTION_MOVE -> {
-//                Log.d(TAG, "Move")
-//                cx = evt.x
-//                cy = evt.y
-//            }
-//        }
-//
-//        invalidate()
-//        return true
-//
-//    }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        var evtAction = event.action
+
+        var x = event.x.toInt()
+        var y = event.y.toInt()
+
+
+
+        when (evtAction) {
+
+            MotionEvent.ACTION_DOWN -> {
+                var btnLeft = (scrW - playBtnUp.width) / 2
+                var btnRight = btnLeft + playBtnUp.width
+                var btnTop = (scrH * 0.5)
+                var btnBottom = btnTop + playBtnUp.height
+
+                var withinBtnBounds = (x > btnLeft) &&
+                        (x < btnRight) &&
+                        (y > btnTop) &&
+                        (y < btnBottom)
+                if (withinBtnBounds) {
+                    playBtnPressed = true
+                }
+
+
+            }
+
+
+            MotionEvent.ACTION_UP -> {
+                if (playBtnPressed) {
+                    // Launch main game screen
+                }
+
+            }
+
+
+            MotionEvent.ACTION_MOVE -> {
+            }
+        }
+
+        invalidate()
+        return true
+
+    }
 
 
 }
